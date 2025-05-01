ea6e8b3eec0a0c45ad0b3de654afcb46359f5261
dacquoise <uberpinkwarrior@gmail.com>
2025-05-01 11:52:18 -0600

    main now gets book text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
