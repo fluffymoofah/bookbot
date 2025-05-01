d752d6f7dcf2dfce25bb2be3afa8a980cea41e29
dacquoise <uberpinkwarrior@gmail.com>
2025-05-01 11:57:19 -0600

    fixed main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(num_words)
+    print(f"{num_words} words found in the document")
 
 def get_num_words(text):
     words = text.split()
